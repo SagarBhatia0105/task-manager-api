0b52a7552faabfad9cb93438863ac9bf4037cb69
Sagar Bhatia <sagar101283cse@gmail.com.com>
2019-06-14 21:44:35 -0400

    Changed mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 console.log(process.env.DATABASE_URL);
+console.log(typeof(process.env.DATABASE_URL));
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser : true,
   useCreateIndex : true
