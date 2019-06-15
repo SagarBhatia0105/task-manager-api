4307108565187697a0713d6379a91bc7c183ec1a
Sagar Bhatia <sagar101283cse@gmail.com.com>
2019-06-14 22:04:11 -0400

    Observing auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,6 +8,8 @@ const auth = async (req, res, next) =>{
       const user = await User.findOne({_id: decoded._id, 'tokens.token': token})
 
       if(!user){
+        console.log("Decoded ", decoded);
+        console.log("Token ", token);
         throw new Error()
       }
 
