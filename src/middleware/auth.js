e35008e1925bc4c2728d1e5410fc71e455434abb
Sagar Bhatia <sagar101283cse@gmail.com.com>
2019-06-27 20:36:24 -0400

    changed auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,6 +6,7 @@ const auth = async (req, res, next) =>{
       const token = req.header('Authorization').replace('Bearer ', '')
       const decoded = jwt.verify(token, process.env.AUTH_TOKEN)
       const user = await User.findOne({_id: decoded._id, 'tokens.token': token})
+      res.status(401).send({token: token, decode: decoded, process.env.AUTH_TOKEN})
 
       if(!user){
         throw new Error(token)
